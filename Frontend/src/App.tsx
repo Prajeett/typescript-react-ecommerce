ec38f4ee140b067013eb5f5f086d20f69ab9723f
Prajeet Basnet <prajitbasnet2000@gmail.com>
2023-10-30 09:11:08 +1000

    added home page and product pages

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,41 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <div className="d-flex flex-column vh-100">
+        <header>
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+              <Navbar.Brand>Prazon</Navbar.Brand>
+            </Container>
+            <Nav>
+              <a href="./cart" className="nav-link">
+                Cart
+              </a>
+
+              <a href="/signin" className="nav-link">
+                Sign In
+              </a>
+            </Nav>
+          </Navbar>
+        </header>
+
+        <main>
+          <Container className="mt-3">
+    <Outlet/>
+  
+            </Container>
+
+        </main>
+        <footer>
+          <div className="text-center">
+            All right reserved
+          </div>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
